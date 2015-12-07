a2497c6d0567ba1caf7f15e76551897a30e24f37
Chandra Mohan Babu Nadiminti <cnadimin@cisco.com>
2015-12-07 17:02:03 -0600

    Support dotfiles for RedHat
    Change-Id: I2c576692f04f605ea064f63bd1173ab83bfa8cfa

---
+++ bin/install_dotfiles.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+read os_name os_info <<< `sh ./os_info.sh`
+
+# Install pip
+if [ "$os_name" = "Darwin" ] ; then
+    if test ! $(which brew); then
+        echo "Installing Homebrew for you."
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    brew install python
+elif [ "$os_name" = "Linux" ] ; then
+    read distro more_info <<<"$os_info"
+    if [ "$distro" = "Debian" ] ; then
+        sudo apt-get install python-pip
+    elif [ "$distro" = "RedHat" ] ; then
+        sudo yum -y install python-pip
+    else
+        echo "Error: Un-expected Linux distribution"
+        exit 1
+    fi
+else
+    echo "Error: Un-expected OS"
+    exit 1
+fi
+
+if test ! $(which dotfiles); then
+    echo "Installing dotfiles using pip..."
+    sudo pip install dotfiles
+fi
+
+cd .. && dotfiles -R . -sf && dotfiles -R . -l
