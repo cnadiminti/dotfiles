8b20bed4d98765b8a4331b895e81cd1367cb44a3
Chandra Mohan Babu Nadiminti <cnadimin@cisco.com>
2015-12-07 22:13:39 -0800

    Updated install.sh
    Change-Id: I66117b0e9ea89ba4f71f120d635b7b13f4388b06

---
+++ bin/install_apps.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+read os_name os_info <<< `sh ./os_info.sh`
+
+# Install all the basic tools and apps
+if [ "$os_name" = "Darwin" ] ; then
+    brew update && brew upgrade
+    brew bundle --global
+    brew bundle cleanup --global
+    brew cleanup && brew cask cleanup
+elif [ "$os_name" = "Linux" ] ; then
+    read distro more_info <<<"$os_info"
+    if [ "$distro" = "Debian" ] ; then
+        sudo apt-get install grc wget
+        # TODO: update the list
+    #elif [ "$distro" = "RedHat" ] ; then
+        #    pkg_mgr='yum'
+    else
+        echo "Error: Un-expected Linux distribution"
+        exit 1
+    fi
+else
+    echo "Error: Un-expected OS"
+    exit 1
+fi
