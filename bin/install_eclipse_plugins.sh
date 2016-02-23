3718d5c154896e05cad5091a20887bade55a448b
Chandra Mohan Babu Nadiminti <cnadimin@cisco.com>
2016-02-23 14:58:23 -0800

    Initial Ruby support
    Change-Id: Id73c78d7731fd114634d1eb394081c3640e85f6e

--- bin/install_eclipse_plugins.sh
+++ bin/install_eclipse_plugins.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 eclipse \
--clean -purgeHistory \
--application org.eclipse.equinox.p2.director \
--profileProperties org.eclipse.update.install.features=true \
--noSplash -repository \
+    -clean -purgeHistory \
+    -application org.eclipse.equinox.p2.director \
+    -profileProperties org.eclipse.update.install.features=true \
+    -noSplash \
+    -repository \
 http://download.eclipse.org/releases/mars/,\
 http://pydev.org/updates/,\
 http://plantuml.sourceforge.net/updatesitejuno/,\
@@ -14,7 +15,7 @@ http://www.nodeclipse.org/updates/markdown/,\
 https://raw.github.com/satyagraha/gfm_viewer/master/p2-composite/,\
 http://workspacemechanic.eclipselabs.org.codespot.com/git.update/mechanic/,\
 http://bcsw.net/plugins/ \
--installIU \
+    -installIU \
 org.python.pydev.feature.feature.group,\
 net.sourceforge.shelled.feature.group,\
 org.dadacoalition.yedit.feature.feature.group,\
@@ -26,5 +27,5 @@ org.eclipse.wst.xml_ui.feature.feature.group,\
 org.eclipse.dltk.tcl.feature.group,\
 net.sourceforge.plantuml.feature.feature.group,\
 org.eclipse.linuxtools.rpm.feature.group,\
-net.bcsw.yang_editor.feature.feature.group
-
+net.bcsw.yang_editor.feature.feature.group,\
+org.eclipse.dltk.ruby.feature.group
