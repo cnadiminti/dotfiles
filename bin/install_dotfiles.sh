c3ea2a84582d53e0ee0f45447449862a205a8737
Chandra Nadiminti <nadiminti.chandra@gmail.com>
2016-05-27 00:26:45 -0700

    explicitly mention dotfiles pip version as 0.6.4

--- bin/install_dotfiles.sh
+++ bin/install_dotfiles.sh
@@ -22,7 +22,7 @@ else
 fi
 
 hash dotfiles 2>/dev/null || \
-    execute_command 'sudo pip install dotfiles' "Installing dotfiles using pip"
+    execute_command 'sudo pip install dotfiles==0.6.4' "Installing dotfiles using pip"
 
 dotfiles -R "${REPO_ROOT_DIR}/dotfiles" -sf
 dotfiles -R "${REPO_ROOT_DIR}/dotfiles" -l
